a969ddff5c021622d035f6c40992ffbf180d71b5
Ross Capdeville <ross@teamengine.org>
2017-05-01 21:55:18 -0700

    Auto commit

--- src/HeapPackage/Main.java
+++ src/HeapPackage/Main.java
@@ -3,8 +3,10 @@ package HeapPackage;
 public class Main {
    public static void main(String[] args) {
       int[] a = {5, 9, 1, 3, 10, 8, 4, 6};
-      
-      
+      System.out.println("");
+      System.out.println("Hello HEAP...");
+      System.out.println("");
+      System.out.println("3rd smallest" + kthSmallest(a, 3));
    }
 
    public static int kthSmallest(int[] a, int k) {
